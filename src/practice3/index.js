a807c61749bea93de8663a1363cb8f25e705621f
Doris8462 <liandou123>
2020-09-14 16:46:16 +0800

    [huangyunjie] feat:complete practice3

--- src/practice3/index.js
+++ src/practice3/index.js
@@ -1,1 +1,26 @@
-export const getPoetry
+export const getPoetry = () => {
+  const url = "https://v1.jinrishici.com/all.json";
+  return fetch(url, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    credentials: "same-origin"
+  })
+    .then(response => response.json())
+    .then(data => {
+      return [data.origin, data.author, data.content];
+    })
+    .catch(err => console.log(err))
+};
+
+// export const getPoetry = async () => {
+//   const url = "https://v1.jinrishici.com/all.json";
+//   try {
+//     const data = await (await fetch(url)).json();
+//     return [data.origin, data.author, data.content];
+//   }
+//   catch (err) {
+//     console.log(err);
+//   }
+// };
