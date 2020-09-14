e2f038c7962a3b8a81a968e145c34eec2c54d99b
Doris8462 <liandou123>
2020-09-14 16:45:18 +0800

    [huangyunjie] feat:complete practice1

--- src/practice1/index.js
+++ src/practice1/index.js
@@ -1,1 +1,6 @@
+import getUrl from "./utils";
+import * as constant from "./constant";
 
+const url = getUrl(constant.ADDRESS, constant.PORT, constant.PATH);
+const region = constant.REGION;
+export { url, region };
