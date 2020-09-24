dfd864708a5bc061fe5841ce737019bf9f8d9423
xijing.gui <xijing.gui@thoughtworks.com>
2020-09-24 14:06:49 +0800

    add feedback

--- src/practice3/index.js
+++ src/practice3/index.js
@@ -1,5 +1,7 @@
 export const getPoetry = () => {
   const url = "https://v1.jinrishici.com/all.json";
+
+  // TODO feedback: 这个场景下，fetch的第二个参数不传也OK的
   return fetch(url, {
     method: "GET",
     headers: {
